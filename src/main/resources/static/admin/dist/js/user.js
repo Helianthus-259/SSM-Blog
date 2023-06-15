628df05cf87c18c043caeb6f05fbe36ecbeb7d3a
戎续亮 <2725501535@qq.com>
2023-06-15 14:57:08 +0800

    更新用户管理

---
+++ src/main/resources/static/admin/dist/js/user.js
@@ -0,0 +1,90 @@
+$(function () {
+    $("#jqGrid").jqGrid({
+        url: '/admin/users/list',
+        datatype: "json",
+        colModel: [
+            {label: 'id', name: 'id', index: 'id', width: 50, key: true, hidden: true},
+            {label: '用户名', name: 'username', index: 'username', width: 60},
+            {label: '密码', name: 'password', index: 'password', width: 60},
+            {label: '头像', name: 'photo', index: 'photo', width: 60},
+            {label: 'github地址', name: 'githubaddress', index: 'githubaddress', width: 70},
+            {label: '创建时间', name: 'createtime', index: 'createtime', width: 70},
+        ],
+        height: 700,
+        rowNum: 18,
+        rowList: [10, 20, 50],
+        styleUI: 'Bootstrap',
+        loadtext: '信息读取中...',
+        rownumbers: false,
+        rownumWidth: 20,
+        autowidth: true,
+        multiselect: true,
+        pager: "#jqGridPager",
+        jsonReader: {
+            root: "data.list",
+            page: "data.currPage",
+            total: "data.totalPage",
+            records: "data.totalCount"
+        },
+        prmNames: {
+            page: "page",
+            rows: "limit",
+            order: "order",
+        },
+        gridComplete: function () {
+            //隐藏grid底部滚动条
+            $("#jqGrid").closest(".ui-jqgrid-bdiv").css({"overflow-x": "hidden"});
+        }
+    });
+    $(window).resize(function () {
+        $("#jqGrid").setGridWidth($(".card-body").width());
+    });
+});
+
+/**
+ * jqGrid重新加载
+ */
+function reload() {
+    var page = $("#jqGrid").jqGrid('getGridParam', 'page');
+    $("#jqGrid").jqGrid('setGridParam', {
+        page: page
+    }).trigger("reloadGrid");
+}
+
+function deleteUser() {
+    var ids = getSelectedRows();
+    if (ids == null) {
+        return;
+    }
+    swal({
+        title: "确认弹框",
+        text: "确认删除这个用户吗?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    }).then((flag) => {
+            if (flag) {
+                $.ajax({
+                    type: "POST",
+                    url: "/admin/users/delete",
+                    contentType: "application/json",
+                    data: JSON.stringify(ids),
+                    success: function (r) {
+                        if (r.resultCode == 200) {
+                            swal("删除成功", {
+                                icon: "success",
+                            });
+                            $("#jqGrid").trigger("reloadGrid");
+                        } else {
+                            alert("删除失败")
+                            alert(r.msg)
+                            swal(r.message, {
+                                icon: "error",
+                            });
+                        }
+                    }
+                });
+            }
+        }
+    );
+}
